1ce13a6cfb9bc3a4405d5295d378c50e4083f984
dwillmore <davidwillmore+gh@gmail.com>
2026-07-14 20:36:22 -0400

    Fixed ghostinggit add irq_charlieplex.c !

--- irq_charlieplex.c
+++ irq_charlieplex.c
@@ -12,10 +12,10 @@ volatile uint32_t brightness[100];
 volatile uint8_t lednum[100];
 volatile uint32_t led_index;
 
-uint32_t get_led_reset( uint32_t led ){
+uint32_t get_led_set( uint32_t led ){
   return(led % (CHARLIE_PINS_N));
 }
-uint32_t get_led_set( uint32_t led ){
+uint32_t get_led_reset( uint32_t led ){
   return((CHARLIE_PINS_N - (led/(CHARLIE_PINS_N-1))) - 1);
 }
 
@@ -25,24 +25,29 @@ void SysTick_Handler(void)
   uint32_t set_pin, reset_pin;
 
 	// Turn off the LED quickly
-	GPIOC->CFGLR = (CHARLIE_CFGLRC_IN & CHARLIE_CFGLRC_MASK);
-	GPIOD->CFGLR = (CHARLIE_CFGLRD_IN & CHARLIE_CFGLRD_MASK);
+//	GPIOC->CFGLR = CHARLIE_CFGLRC_IN;
+//	GPIOD->CFGLR = CHARLIE_CFGLRD_IN;
  	// Clear the output values for our pins
  	GPIOC->BSHR = CHARLIE_BSHRC_PINS;
  	GPIOD->BSHR = CHARLIE_BSHRD_PINS;
+	// And drive them all low to prevent ghosting
+	GPIOC->CFGLR = CHARLIE_CFGLRC_OUT;
+//	GPIOC->CFGLR = CHARLIE_CFGLRC_IN;
+	GPIOD->CFGLR = CHARLIE_CFGLRD_OUT;
+//	GPIOD->CFGLR = CHARLIE_CFGLRD_IN;
 
 	// If the current led_index points to an invalid LED, don't display anything
 	if(lednum[led_index] < CHARLIE_LEDS){
 		// calculate next values to set
-		reset_pin = get_led_set( lednum[led_index] );
-  	set_pin = get_led_reset( lednum[led_index] );
+		reset_pin = get_led_reset( lednum[led_index] );
+  	set_pin = get_led_set( lednum[led_index] );
 
   	// Configure the pins for input/output
-  	GPIOC->CFGLR |= charlie_pin_data[set_pin].cfglrc | charlie_pin_data[reset_pin].cfglrc;
-  	GPIOD->CFGLR |= charlie_pin_data[set_pin].cfglrd | charlie_pin_data[reset_pin].cfglrd;
+  	GPIOC->CFGLR = charlie_pin_data[set_pin].cfglrc | charlie_pin_data[reset_pin].cfglrc;
+  	GPIOD->CFGLR = charlie_pin_data[set_pin].cfglrd | charlie_pin_data[reset_pin].cfglrd;
 
   	// Set the set pin
-  	GPIOC->BSHR = charlie_pin_data[set_pin].bshrc;
+		GPIOC->BSHR = charlie_pin_data[set_pin].bshrc;
   	GPIOD->BSHR = charlie_pin_data[set_pin].bshrd;
 	}
 
@@ -80,8 +85,8 @@ int main()
 		lednum[index] = i;
 //		brightness[index] = DELAY_MS_TIME/150;
 //		total_brightness += DELAY_MS_TIME/150;
-		brightness[index] = 275;
-		total_brightness += 275;
+		brightness[index] = 400;
+		total_brightness += 400;
 		index++;
 	}
 
@@ -92,9 +97,9 @@ int main()
 	lednum[index] = 255;
 
 	// Setup the systick IRQ and set it to fire in one MS
-	SysTick->CTLR = 0x0000;
-  SysTick->CMP = DELAY_MS_TIME - 1;
-  SysTick->CNT = 0x00000000;
+	SysTick->CTLR = 0;
+  SysTick->CMP = 1000;
+  SysTick->CNT = 0;
   SysTick->CTLR |= SYSTICK_CTLR_STE | SYSTICK_CTLR_STIE | SYSTICK_CTLR_STCLK ; 
   NVIC_EnableIRQ(SysTick_IRQn);
 
